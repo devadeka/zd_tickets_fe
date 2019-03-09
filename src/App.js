1aaa1c7c4e1618a28c66d1d9a28f57d008809a6d
Devatanu Deka <deva.deka@gmail.com>
2019-03-09 20:55:30 +1100

    TicketDetailView test and implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import '@zendeskgarden/react-chrome/dist/styles.css';
 import './App.css';
+
 import {
   Body,
   Chrome,
   Content
 } from '@zendeskgarden/react-chrome';
-import React from 'react';
+
+import React, { useState } from 'react';
 import TicketDetailView from './components/TicketDetailView';
 import TicketsPanel from './components/TicketsPanel';
 import TitleBar from './components/TitleBar';
 
 const App = () => {
+  const [title, setTitle] = useState('Select Ticket');
+  const [content, setContent] = useState('To view the details of a ticket please click on one from our side menu.');
+
   return (
     <Chrome className="App">
       <Body>
         <TitleBar />
         <Content>
           <TicketsPanel />
-          <TicketDetailView />
+          <TicketDetailView title={ title } content ={ content }/>
         </Content>
       </Body>
     </Chrome>
