a79cfeb6494b57db4416cc1b5079fb528e5fe0a8
Devatanu Deka <deva.deka@gmail.com>
2019-03-09 13:44:04 +1100

    base app working with ZD_Garden

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { 
+    Chrome, Body, Content, Sidebar, Main,
+    Header, HeaderItemWrapper
+  } from '@zendeskgarden/react-chrome'
+import '@zendeskgarden/react-chrome/dist/styles.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = () => {
+  return (
+      <Chrome className="App">
+        <Body>
+          <Header>
+            <HeaderItemWrapper maxX>
+              <h1>Zendesk Ticket View</h1>
+            </HeaderItemWrapper>
+          </Header>
+          <Content>
+            <Sidebar style={{ padding: 28 }}>
+              <h2>Example Sidebar</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </p>
+              <p>
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.
+              </p>
+            </Sidebar>
+            <Main style={{ padding: 28 }}>
+              <h2>Main Content</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </p>
+              <p>
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.
+              </p>
+            </Main>
+          </Content>
+        </Body>
+      </Chrome>
+  );
 }
 
 export default App;
